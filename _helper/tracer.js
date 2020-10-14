0e0304fd2fed5d23787c21f203fd942aedfe34a8
Andres Somadossi <andressomadossi@gmail.com>
2020-10-14 15:53:18 -0300

    tracer flag enabled

--- _helper/tracer.js
+++ _helper/tracer.js
@@ -1,12 +1,12 @@
-process.env.AWS_XRAY_CONTEXT_MISSING = 'LOG_ERROR';
+const disabled = (process.env.EBASED_DISABLE_X_RAY == 'true') ? true : false;
 const AWSXRay = require('aws-xray-sdk-core');
 
 module.exports = {
   captureAWSClient: (client) => {
-    return (!process.env.IS_LOCAL) ? AWSXRay.captureAWSClient(client) : client;
+    return (!process.env.IS_LOCAL && !disabled) ? AWSXRay.captureAWSClient(client) : client;
   },
   captureHTTP: () => {
-    if (!process.env.IS_LOCAL) {
+    if (!process.env.IS_LOCAL && !disabled) {
       AWSXRay.captureHTTPsGlobal(require('http'));
       AWSXRay.captureHTTPsGlobal(require('https'));
     }
